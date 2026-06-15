e62b0c0862162df91f1e7ec58c56f7ab9e986bdb
Raylla Ruthiely <rayllaruthielly707616@gmail.com>
2026-06-15 18:42:19 -0300

    documentação

--- projetoXadrez/src/tabuleiro/ExcecaoTabuleiro.java
+++ projetoXadrez/src/tabuleiro/ExcecaoTabuleiro.java
@@ -1,8 +1,21 @@
 package tabuleiro;
 
+/**
+ * Exceção personalizada para erros relacionados ao tabuleiro.
+ * É lançada quando ocorre uma operação inválida, como acessar
+ * uma posição inexistente ou tentar colocar uma peça em uma
+ * posição já ocupada.
+ */
+
 public class ExcecaoTabuleiro extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Cria uma exceção do tabuleiro com a mensagem informada.
+	 *
+	 * @param mensagem descrição do erro ocorrido
+	 */
 
 	public ExcecaoTabuleiro(String mensagem) {
 		super(mensagem);
