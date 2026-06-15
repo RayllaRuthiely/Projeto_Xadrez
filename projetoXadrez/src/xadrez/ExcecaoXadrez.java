7993dd13aab148de53361b7d124a9b7bd466f3e1
Raylla Ruthiely <rayllaruthielly707616@gmail.com>
2026-06-15 18:54:45 -0300

    documentação

--- projetoXadrez/src/xadrez/ExcecaoXadrez.java
+++ projetoXadrez/src/xadrez/ExcecaoXadrez.java
@@ -1,12 +1,22 @@
 package xadrez;
 
+/**
+ * Classe de exceção específica do jogo de xadrez.
+ * É utilizada para indicar erros relacionados às regras
+ * da partida, como movimentos inválidos ou jogadas ilegais.
+ */
+
 import tabuleiro.ExcecaoTabuleiro;
 
 public class ExcecaoXadrez extends ExcecaoTabuleiro {
 
 	private static final long serialVersionUID = 1L;
 
-	// Construtor
+	/**
+	 * Construtor da exceção de xadrez.
+	 *
+	 * @param mensagem Mensagem que descreve o erro ocorrido.
+	 */
 	public ExcecaoXadrez(String mensagem) {
 		super(mensagem);
 	}
